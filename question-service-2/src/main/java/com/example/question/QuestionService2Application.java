f62084c8b86aa6af096c6068a9239beac853d6d2
Rakesh8086 <rakeshteja7007@gmail.com>
2025-11-29 15:41:56 +0530

    Nov29 - Config server created and DB config details shifted to config repo. Tested and working

--- question-service-2/src/main/java/com/example/question/QuestionService2Application.java
+++ question-service-2/src/main/java/com/example/question/QuestionService2Application.java
@@ -1,13 +1,25 @@
 package com.example.question;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuestionService2Application {
+public class QuestionService2Application implements CommandLineRunner { // Implement CommandLineRunner
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuestionService2Application.class, args);
-	}
+    @Value("${custom.config.source}")
+    private String configSource;
 
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionService2Application.class, args);
+    }
+
+    // This method runs once the application is started
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("---------------------------------------------");
+        System.out.println("CONFIG SOURCE TEST VALUE: " + configSource);
+        System.out.println("---------------------------------------------");
+    }
 }
--- quiz-service-1/src/main/java/com/example/quiz/QuizService1Application.java
+++ quiz-service-1/src/main/java/com/example/quiz/QuizService1Application.java
@@ -1,15 +1,27 @@
 package com.example.quiz;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class QuizService1Application {
+public class QuizService1Application implements CommandLineRunner { // Implement CommandLineRunner
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizService1Application.class, args);
-	}
+    @Value("${custom.config.source}")
+    private String configSource;
 
+    public static void main(String[] args) {
+        SpringApplication.run(QuizService1Application.class, args);
+    }
+
+    // This method runs once the application is started
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("---------------------------------------------");
+        System.out.println("CONFIG SOURCE TEST VALUE: " + configSource);
+        System.out.println("---------------------------------------------");
+    }
 }
